0eea80f14f852207ebadb84bc992911f57304c01
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-04-05 15:26:34 +0700

    model kategori

---
+++ application/models/M_kategori.php
@@ -0,0 +1,46 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_kategori extends CI_Model
+{
+
+	// List all your items
+	public function kategori()
+	{
+		$this->db->select('*');
+		$this->db->from('kategori');
+		$this->db->order_by('id_kategori', 'desc');
+		return $this->db->get()->result();
+	}
+
+	// Add a new item
+	public function add($data)
+	{
+		$this->db->insert('kategori', $data);
+	}
+
+	//Update one item
+	public function update($data)
+	{
+		$this->db->where('id_kategori', $data['id_kategori']);
+		$this->db->update('kategori', $data);
+	}
+
+	public function detail_kategori($id_kategori = null)
+	{
+		$this->db->select('*');
+		$this->db->from('kategori');
+		$this->db->where('id_kategori', $id_kategori);
+		return $this->db->get()->row();
+	}
+
+	//Delete one item
+	public function delete($data)
+	{
+		$this->db->where('id_kategori', $data['id_kategori']);
+		$this->db->delete('kategori', $data);
+	}
+}
+
+/* End of file Controllername.php */
