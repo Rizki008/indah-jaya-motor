90d6ed0838ff1353d89979dea2d5c09383fdebfc
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-04-30 17:31:23 +0700

    library proses login pelanggan

---
+++ application/libraries/Pelanggan_login.php
@@ -0,0 +1,69 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Pelanggan_login
+{
+	protected $ci;
+
+	public function __construct()
+	{
+		$this->ci = &get_instance();
+		$this->ci->load->model('m_auth');
+	}
+
+	public function login($email, $password)
+	{
+		$cek = $this->ci->m_auth->pelanggan_login($email, $password);
+
+		if ($cek) {
+			$id_pelanggan = $cek->id_pelanggan;
+			$nm_pel = $cek->nm_pel;
+			$email = $cek->email;
+			$password = $cek->password;
+			// $alamat = $cek->alamat;
+			$no_tlpon = $cek->no_tlpon;
+			// $kode_pos = $cek->kode_pos;
+			$point = $cek->point;
+			$level_member = $cek->level_member;
+
+			$this->ci->session->set_userdata('id_pelanggan', $id_pelanggan);
+			$this->ci->session->set_userdata('nm_pel', $nm_pel);
+			$this->ci->session->set_userdata('password', $password);
+			$this->ci->session->set_userdata('email', $email);
+			// $this->ci->session->set_userdata('alamat', $alamat);
+			$this->ci->session->set_userdata('no_tlpon', $no_tlpon);
+			// $this->ci->session->set_userdata('kode_pos', $kode_pos);
+			$this->ci->session->set_userdata('point', $point);
+			$this->ci->session->set_userdata('level_member', $level_member);
+
+			redirect('home');
+		} else {
+			$this->ci->session->set_flashdata('error', 'Email atau Password Error');
+			redirect('pelanggan/login');
+		}
+	}
+
+	public function proteksi_halaman()
+	{
+		if ($this->ci->session->userdata('email') == '') {
+			$this->ci->session->set_flashdata('error', 'Anda belum Login!!!');
+			redirect('pelanggan/login');
+		}
+	}
+
+	public function logout()
+	{
+		$this->ci->session->unset_userdata('id_pelanggan');
+		$this->ci->session->unset_userdata('nm_pel');
+		$this->ci->session->unset_userdata('password');
+		$this->ci->session->unset_userdata('email');
+		// $this->ci->session->unset_userdata('alamat');
+		$this->ci->session->unset_userdata('no_tlpon');
+		// $this->ci->session->unset_userdata('kode_pos');
+		$this->ci->session->unset_userdata('point');
+		$this->ci->session->unset_userdata('level_member');
+		$this->ci->session->set_flashdata('pesan', 'Berhasil Logout!!!');
+		redirect('pelanggan/login');
+	}
+}
