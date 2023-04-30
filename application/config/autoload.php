2a069f3eedd0bc6d13cb7b32530512ab4e339f8a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-04-30 17:41:00 +0700

    menambahkan library

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation', 'database', 'upload');
+$autoload['libraries'] = array('session', 'form_validation', 'database', 'cart', 'upload', 'pelanggan_login', 'user_login');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form', 'file', 'string');
+$autoload['helper'] = array('url', 'form', 'global', 'file', 'string');
 
 /*
 | -------------------------------------------------------------------
